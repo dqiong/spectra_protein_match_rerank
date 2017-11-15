b5f38f130b7fd29e77ac6b654a665bc71e028910
dqiong <duanqiong1215@163.com>
2017-11-15 22:12:51 +0800

    更新readPSM函数

--- psmAlign.py
+++ psmAlign.py
@@ -19,8 +19,9 @@ class PSM:
     matched_fragment_ions=0
     p_value=0
     e_value=0
+    protein_taget_decoy="None"
     def __init__(self, psm_id, spectra_id, peaks, charge, precursor_mass, protein_id,protein_name, protein_mass,
-        first_residue,last_residue,modification_number, matched_peaks, matched_fragment_ions, p_value,e_value):
+        first_residue,last_residue,modification_number, matched_peaks, matched_fragment_ions, p_value,e_value,protein_flag):
         self.psm_id=psm_id
         self.spectra_id=spectra_id
         self.peaks=peaks
@@ -32,10 +33,11 @@ class PSM:
         self.first_residue=first_residue
         self.last_residue=last_residue
         self.modification_number=modification_number
-        self.matched_peaks=matched_peaks
-        self.matched_fragment_ions=matched_fragment_ions
-        self.p_value=p_value
-        self.e_value=e_value
+        self.matched_peaks=float(matched_peaks)
+        self.matched_fragment_ions=float(matched_fragment_ions)
+        self.p_value=float(p_value)
+        self.e_value=float(e_value)
+        self.protein_taget_decoy=protein_flag
     def __cmp__(self,other):  
         return cmp(self.matched_peaks, other.matched_peaks)  
 def readPSM():
@@ -44,10 +46,14 @@ def readPSM():
     input_file = open(unicode(input_file_path, "utf-8"), "r")
     for line in islice(input_file, 1, None):
         tokens = line.strip().replace(" ", "\t").split("\t")
+        protein_flag=tokens[9].split("_")[0]
         psm=PSM(tokens[1],tokens[2],tokens[4],tokens[5],tokens[6],tokens[8],tokens[9],tokens[10],tokens[11],tokens[12],tokens[14],tokens[15],tokens[16],
-        tokens[17],tokens[18])
+        tokens[17],tokens[18],protein_flag)
         psm_list.append(psm)
     print "number of psm:",len(psm_list)
+    psm_list.sort(reverse=True)
+    print psm_list[0].matched_peaks,psm_list[len(psm_list)-1].matched_peaks
+
 
 if __name__ == "__main__":
     readPSM()   
